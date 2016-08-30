22e44e0d928915d19c1ba8d19b77e5d7f9be9509
Liam Flood <stonga.fedora@gmail.com>
2016-08-30 14:42:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+//INSTRUCTIONS FROM FCC
+//
+//var express = require('express');
+//var app = express();
+//
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
+//
+//app.listen(8080, function () {
+//  console.log('Example app listening on port 8080!');
+//});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+var http= require('http');
+
+var server = http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type: 'text/html'})
+  res.end('<h1>Hello World</h1>');
+  
+  
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+var port = Number(process.env.PORT||3000)
+
+server.listen(port);
